dcd833a9076a4db78f1700195fe815977e4e5e3a
Paul La Plante <plaplant@sas.upenn.edu>
2018-02-15 11:55:06 -0700

    Add all-in-one cleaner script

---
+++ scripts/clean_up_makeflow.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2.7
+# -*- mode: python; coding: utf-8 -*-
+# Copyright (c) 2018 The HERA Collaboration
+
+from __future__ import print_function, division, absolute_import
+import os
+from hera_op import mf_tools as mt
+from hera_op import utils
+
+a = utils.get_cleaner_ArgumentParser('logs')
+# change program name to reflect script
+a.prog = "clean_up_makeflow.py"
+args = a.parse_args()
+work_dir = args.directory
+
+# call wrapper cleaner function
+print("Cleaning wrapper scripts in {}".format(work_dir))
+mt.clean_wrapper_scripts(work_dir)
+
+# call output cleaner function
+print("Cleaning output files in {}".format(work_dir))
+mt.clean_output_files(work_dir)
+
+# call log-making function
+output = args.output
+overwrite = args.overwrite
+remove_original = args.remove_original
+zip_output = args.zip
+
+print("Consolidating log files in {}".format(work_dir))
+mt.consolidate_logs(work_dir, output, overwrite, remove_original, zip_output)
