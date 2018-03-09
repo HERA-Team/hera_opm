3e3b6cf26b6f9bad5fa3c1cbac74bf88d8142555
Adam Beardsley <adam.p.beardsley@gmail.com>
2018-03-09 10:19:25 -0700

    update do_DELAY_XRFI_APPLY to pass in args

--- hera_opm/data/idr2/task_scripts/do_DELAY_XRFI_APPLY.sh
+++ hera_opm/data/idr2/task_scripts/do_DELAY_XRFI_APPLY.sh
@@ -13,6 +13,16 @@ pol2="yy"
 pol3="xy"
 pol4="yx"
 
+# This script will take in all the waterfalls produced by the various DELAY_XRFI
+# threads, union them, and apply to the data. It will produce a flagged data file
+# and an npz file with the flag array for this data set.
+
+# Parameters are set in the configuration file, here we define their positions,
+# which must be consistent with the config.
+# 1 - filename
+# 2 - extension
+# 3 - output_npz_ext
+
 # get raw visibility waterfalls
 base_xx=$(replace_pol ${fn} ${pol1})
 base_yy=$(replace_pol ${fn} ${pol2})
@@ -40,5 +50,5 @@ g_wf_yy=`echo ${base_yy}.abs.calfits.g.flags.npz`
 wf_list=$(join_by , ${wf_xx} ${wf_yy} ${wf_xy} ${wf_yx} ${vis_wf_xx} ${vis_wf_yy} ${chi_wf_xx} ${chi_wf_yy} ${g_wf_xx} ${g_wf_yy})
 
 # build xrfi_apply commmand
-echo xrfi_apply.py --infile_format=miriad --outfile_format=miriad --extension=R --overwrite --output_npz --out_npz_ext=.flags.applied.npz --waterfalls=${wf_list} ${fn}OC
-xrfi_apply.py --infile_format=miriad --outfile_format=miriad --extension=R --overwrite --output_npz --out_npz_ext=.flags.applied.npz --waterfalls=${wf_list} ${fn}OC
+echo xrfi_apply.py --extension=${2} --overwrite --out_npz_ext=${3} --waterfalls=${wf_list} ${fn}OC
+xrfi_apply.py --extension=${2} --overwrite --out_npz_ext=${3} --waterfalls=${wf_list} ${fn}OC
