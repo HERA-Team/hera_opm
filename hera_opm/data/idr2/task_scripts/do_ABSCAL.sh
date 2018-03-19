f902b8cf6068e1f447efc786935f4574b042e097
Nicholas Kern <nkern@berkeley.edu>
2018-03-19 14:08:16 -0700

    updated idr1 config for ABSCAL

--- hera_opm/data/idr2/task_scripts/do_ABSCAL.sh
+++ hera_opm/data/idr2/task_scripts/do_ABSCAL.sh
@@ -8,9 +8,9 @@ source ${src_dir}/_common.sh
 # Parameters are set in the configuration file, here we define their positions,
 # which must be consistent with the config.
 # 1 - filename
-# 2 - glob-parsable string pointing to xx model_files
+# 2 - glob-parsable string pointing to model_files
 fn="${1}"
-xx_model_files="${2}"
+model_files="${2}"
 
 # we only want to run on linear polarizations (e.g., "xx")
 if is_lin_pol $fn; then
@@ -20,9 +20,12 @@ if is_lin_pol $fn; then
 
     # substitute polarization of filename into model file list
     pol=$(get_pol ${fn})
-    model_files=`echo ${xx_model_files} | sed -E "s/\.xx\./.$pol./g"`
+    model_files=`echo ${model_files} | sed -E "s/\.pp\./.$pol./g"`
+
+    # make filepath strings
+    omni_data=${fn}O
 
     # call omni-abscal script; see hera_cal.abscal for more details
-    echo omni_abscal_run.py --delay_slope_cal --TT_phs_cal --abs_amp_cal --overwrite --calfits_infiles ${omni_fn} --output_calfits_fname ${calfits_fn} --data_files ${fn}O --model_files ${model_files}
-    omni_abscal_run.py --delay_slope_cal --TT_phs_cal --abs_amp_cal --overwrite --calfits_infiles ${omni_fn} --output_calfits_fname ${calfits_fn} --data_files ${fn}O --model_files ${model_files}
+    echo omni_abscal_run.py --delay_slope_cal --TT_phs_cal --abs_amp_cal --overwrite --calfits_infiles ${omni_fn} --output_calfits_fname ${calfits_fn} --data_files ${omni_data} --model_files ${model_files}
+    omni_abscal_run.py --delay_slope_cal --TT_phs_cal --abs_amp_cal --overwrite --calfits_infiles ${omni_fn} --output_calfits_fname ${calfits_fn} --data_files ${omni_data} --model_files ${model_files}
 fi
