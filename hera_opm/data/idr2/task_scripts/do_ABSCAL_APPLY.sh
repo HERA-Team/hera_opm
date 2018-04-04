b47ef9f6440be38a952e9791319e1076c5bc852b
Josh Dillon <jsdiillon@berkeley.edu>
2018-04-04 16:34:07 -0700

    add parametrized band edge flagging to pipeline

--- hera_opm/data/idr2/task_scripts/do_ABSCAL_APPLY.sh
+++ hera_opm/data/idr2/task_scripts/do_ABSCAL_APPLY.sh
@@ -5,15 +5,18 @@ set -e
 src_dir="$(dirname "$0")"
 source ${src_dir}/_common.sh
 
-# get base filename
+# Positional parameters passed as defined in the configuration file:
+# 1 - base filename
+# 2 - flag_nchan_low (n low frequency channels to flag)
+# 3 - flag_nchan_high (n high frequency channels to flag)
 fn=${1}
 
 if is_lin_pol ${fn}; then
     # we only need the corresponding omnical solution
     abs_fn=`echo ${fn}.abs.calfits`
 
-    echo apply_cal.py ${fn} ${fn}OC --new_cal ${abs_fn} --clobber --filetype=miriad --gain_convention=divide
-    apply_cal.py ${fn} ${fn}OC --new_cal ${abs_fn} --clobber --filetype=miriad --gain_convention=divide
+    echo apply_cal.py ${fn} ${fn}OC --new_cal ${abs_fn} --flag_nchan_low=${2} --flag_nchan_high=${3} --clobber --filetype=miriad --gain_convention=divide
+    apply_cal.py ${fn} ${fn}OC --new_cal ${abs_fn} --flag_nchan_low=${2} --flag_nchan_high=${3} --clobber --filetype=miriad --gain_convention=divide
 else
     # we need both omnical solutions
     fn_xx=$(replace_pol $fn "xx")
@@ -21,6 +24,6 @@ else
     abs_xx=`echo ${fn_xx}.abs.calfits`
     abs_yy=`echo ${fn_yy}.abs.calfits`
 
-    echo apply_cal.py ${fn} ${fn}OC --new_cal ${abs_xx} ${abs_yy} --clobber --filetype=miriad --gain_convention=divide
-    apply_cal.py ${fn} ${fn}OC --new_cal ${abs_xx} ${abs_yy} --clobber --filetype=miriad --gain_convention=divide
+    echo apply_cal.py ${fn} ${fn}OC --new_cal ${abs_xx} ${abs_yy} --flag_nchan_low=${2} --flag_nchan_high=${3} --clobber --filetype=miriad --gain_convention=divide
+    apply_cal.py ${fn} ${fn}OC --new_cal ${abs_xx} ${abs_yy} --flag_nchan_low=${2} --flag_nchan_high=${3} --clobber --filetype=miriad --gain_convention=divide
 fi
--- hera_opm/data/idr2/task_scripts/do_OMNI_APPLY.sh
+++ hera_opm/data/idr2/task_scripts/do_OMNI_APPLY.sh
@@ -5,15 +5,18 @@ set -e
 src_dir="$(dirname "$0")"
 source ${src_dir}/_common.sh
 
-# get base filename
+# Positional parameters passed as defined in the configuration file:
+# 1 - base filename
+# 2 - flag_nchan_low (n low frequency channels to flag)
+# 3 - flag_nchan_high (n high frequency channels to flag)
 fn=${1}
 
 if is_lin_pol ${fn}; then
     # we only need the corresponding omnical solution
     omni_fn=`echo ${fn}.omni.calfits`
 
-    echo apply_cal.py ${fn} ${fn}O --new_cal ${omni_fn} --clobber --filetype=miriad --gain_convention=divide
-    apply_cal.py ${fn} ${fn}O --new_cal ${omni_fn} --clobber --filetype=miriad --gain_convention=divide
+    echo apply_cal.py ${fn} ${fn}O --new_cal ${omni_fn} --clobber --flag_nchan_low=${2} --flag_nchan_high=${3} --filetype=miriad --gain_convention=divide
+    apply_cal.py ${fn} ${fn}O --new_cal ${omni_fn} --clobber --flag_nchan_low=${2} --flag_nchan_high=${3} --filetype=miriad --gain_convention=divide
 else
     # we need both omnical solutions
     fn_xx=$(replace_pol $fn "xx")
@@ -21,6 +24,6 @@ else
     omni_xx=`echo ${fn_xx}.omni.calfits`
     omni_yy=`echo ${fn_yy}.omni.calfits`
 
-    echo apply_cal.py ${fn} ${fn}O --new_cal ${omni_xx} ${omni_yy} --clobber --filetype=miriad --gain_convention=divide
-    apply_cal.py ${fn} ${fn}O --new_cal ${omni_fn} ${omni_yy} --clobber --filetype=miriad --gain_convention=divide
+    echo apply_cal.py ${fn} ${fn}O --new_cal ${omni_xx} ${omni_yy} --flag_nchan_low=${2} --flag_nchan_high=${3} --clobber --filetype=miriad --gain_convention=divide
+    apply_cal.py ${fn} ${fn}O --new_cal ${omni_fn} ${omni_yy} --flag_nchan_low=${2} --flag_nchan_high=${3} --clobber --filetype=miriad --gain_convention=divide
 fi
