da94ada058f1907766a85bb51e2406f67a8d9de9
Josh Dillon <jsdiillon@berkeley.edu>
2018-04-20 14:26:02 -0700

    Update do_abscal and config for new abscal argparser and arguments related to TT_phs_cal

--- hera_opm/data/idr2/task_scripts/do_ABSCAL.sh
+++ hera_opm/data/idr2/task_scripts/do_ABSCAL.sh
@@ -9,8 +9,13 @@ source ${src_dir}/_common.sh
 # which must be consistent with the config.
 # 1 - filename
 # 2 - glob-parsable string pointing to model_files
+# 3 - integer maximum number of iterations of TT_phs_cal allowed
+# 4 - float convergence criterion in Delta g / g for stopping iterative TT_phs_cal
+
 fn="${1}"
 model_files="${2}"
+TT_phs_max_iter="${3}"
+TT_phs_conv_crit="${4}"
 
 # we only want to run on linear polarizations (e.g., "xx")
 if is_lin_pol $fn; then
@@ -26,6 +31,6 @@ if is_lin_pol $fn; then
     omni_data=${fn}O
 
     # call omni-abscal script; see hera_cal.abscal for more details
-    echo omni_abscal_run.py --delay_slope_cal --TT_phs_cal --abs_amp_cal --overwrite --calfits_infiles ${omni_fn} --output_calfits_fname ${calfits_fn} --data_files ${omni_data} --model_files ${model_files}
-    omni_abscal_run.py --delay_slope_cal --TT_phs_cal --abs_amp_cal --overwrite --calfits_infiles ${omni_fn} --output_calfits_fname ${calfits_fn} --data_files ${omni_data} --model_files ${model_files}
+    echo omni_abscal_run.py --delay_slope_cal --phase_slope_cal --TT_phs_cal --abs_amp_cal --TT_phs_max_iter ${TT_phs_max_iter} --TT_phs_conv_crit ${TT_phs_conv_crit} --overwrite --calfits_infile ${omni_fn} --output_calfits_fname ${calfits_fn} --data_file ${omni_data} --model_files ${model_files} 
+    omni_abscal_run.py --delay_slope_cal --phase_slope_cal --TT_phs_cal --abs_amp_cal --TT_phs_max_iter ${TT_phs_max_iter} --TT_phs_conv_crit ${TT_phs_conv_crit} --overwrite --calfits_infile ${omni_fn} --output_calfits_fname ${calfits_fn} --data_file ${omni_data} --model_files ${model_files} 
 fi
