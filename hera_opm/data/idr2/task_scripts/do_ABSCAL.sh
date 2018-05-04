1be21d6da2baa7594704332b367807cb7bdabe62
Josh Dillon <jsdiillon@berkeley.edu>
2018-05-04 16:55:43 -0700

    Update parameters for iterative phase cal

--- hera_opm/data/idr2/task_scripts/do_ABSCAL.sh
+++ hera_opm/data/idr2/task_scripts/do_ABSCAL.sh
@@ -9,16 +9,16 @@ source ${src_dir}/_common.sh
 # which must be consistent with the config.
 # 1 - filename
 # 2 - glob-parsable string pointing to model_files
-# 3 - integer maximum number of iterations of TT_phs_cal allowed
-# 4 - float convergence criterion in Delta g / g for stopping iterative TT_phs_cal
+# 3 - integer maximum number of iterations of phase calibration algorithms allowed
+# 4 - float convergence criterion in Delta g / g for stopping iterative phase_slope_cal or TT_phs_cal
 # 5 - reference antenna for phase solvers
 # 6 - enact a cut on max baseline length for data that goes into calibration equations
 # 7 - number of channels on each edge of bandpass to flag before delay calibration
 
 fn="${1}"
 model_files="${2}"
-TT_phs_max_iter="${3}"
-TT_phs_conv_crit="${4}"
+phs_max_iter="${3}"
+phs_conv_crit="${4}"
 refant="${5}"
 bl_cut="${6}"
 edge_cut="${7}"
@@ -37,6 +37,6 @@ if is_lin_pol $fn; then
     omni_data=${fn}O
 
     # call omni-abscal script; see hera_cal.abscal for more details
-    echo omni_abscal_run.py --refant ${refant} --bl_cut ${bl_cut} --edge_cut ${edge_cut} --avg_dly_slope_cal --delay_slope_cal --phase_slope_cal --TT_phs_cal --abs_amp_cal --TT_phs_max_iter ${TT_phs_max_iter} --TT_phs_conv_crit ${TT_phs_conv_crit} --overwrite --calfits_infile ${omni_fn} --output_calfits_fname ${calfits_fn} --data_file ${omni_data} --model_files ${model_files} 
-    omni_abscal_run.py --refant ${refant} --bl_cut ${bl_cut} --edge_cut ${edge_cut} --avg_dly_slope_cal --delay_slope_cal --phase_slope_cal --TT_phs_cal --abs_amp_cal --TT_phs_max_iter ${TT_phs_max_iter} --TT_phs_conv_crit ${TT_phs_conv_crit} --overwrite --calfits_infile ${omni_fn} --output_calfits_fname ${calfits_fn} --data_file ${omni_data} --model_files ${model_files} 
+    echo omni_abscal_run.py --refant ${refant} --bl_cut ${bl_cut} --edge_cut ${edge_cut} --avg_dly_slope_cal --delay_slope_cal --phase_slope_cal --TT_phs_cal --abs_amp_cal --phs_max_iter ${phs_max_iter} --phs_conv_crit ${phs_conv_crit} --overwrite --calfits_infile ${omni_fn} --output_calfits_fname ${calfits_fn} --data_file ${omni_data} --model_files ${model_files} 
+    omni_abscal_run.py --refant ${refant} --bl_cut ${bl_cut} --edge_cut ${edge_cut} --avg_dly_slope_cal --delay_slope_cal --phase_slope_cal --TT_phs_cal --abs_amp_cal --phs_max_iter ${phs_max_iter} --phs_conv_crit ${phs_conv_crit} --overwrite --calfits_infile ${omni_fn} --output_calfits_fname ${calfits_fn} --data_file ${omni_data} --model_files ${model_files} 
 fi
