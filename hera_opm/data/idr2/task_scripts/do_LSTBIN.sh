c9bfa879c813d6e7f436849ee55aa35fa56524f7
Nicholas Kern <nkern@berkeley.edu>
2018-05-30 21:38:23 -0700

    added file_ext to lstbin kwargs 	modified:   hera_opm/data/idr2/idr2_lstbin.cfg 	modified:   hera_opm/data/idr2/task_scripts/do_LSTBIN.sh

--- hera_opm/data/idr2/task_scripts/do_LSTBIN.sh
+++ hera_opm/data/idr2/task_scripts/do_LSTBIN.sh
@@ -15,8 +15,9 @@ source ${src_dir}/_common.sh
 # 6 - lst_start
 # 7 - vis_units
 # 8 - output_file_select
-# 9 - outdir
-# 10+ - series of glob-parsable search strings (in quotations!) to files to LSTBIN
+# 9 - file_ext
+# 10 - outdir
+# 11+ - series of glob-parsable search strings (in quotations!) to files to LSTBIN
 
 # get positional arguments
 sig_clip=${1}
@@ -27,9 +28,10 @@ ntimes_per_file=${5}
 lst_start=${6}
 vis_units=${7}
 output_file_select=${8}
-outdir=${9}
+file_ext=${9}
+outdir=${10}
 data_files=($@)
-data_files=(${data_files[*]:9})
+data_files=(${data_files[*]:10})
 
 # set special kwargs
 if [ $sig_clip == True ]; then
@@ -43,5 +45,5 @@ else
     rephase=""
 fi
 
-echo lstbin_run.py --outdir ${outdir} --ntimes_per_file ${ntimes_per_file} --overwrite ${sig_clip} --sigma ${sigma} --min_N ${min_N} --lst_start ${lst_start} --vis_units ${vis_units} --output_file_select ${output_file_select} ${data_files[@]}
-lstbin_run.py --outdir ${outdir} --ntimes_per_file ${ntimes_per_file} --overwrite ${sig_clip} --sigma ${sigma} --min_N ${min_N} --lst_start ${lst_start} --vis_units ${vis_units} --output_file_select ${output_file_select} ${data_files}
+echo lstbin_run.py --file_ext ${file_ext} --outdir ${outdir} --ntimes_per_file ${ntimes_per_file} --overwrite ${sig_clip} --sigma ${sigma} --min_N ${min_N} --lst_start ${lst_start} --vis_units ${vis_units} --output_file_select ${output_file_select} ${data_files[@]}
+lstbin_run.py --file_ext ${file_ext} --outdir ${outdir} --ntimes_per_file ${ntimes_per_file} --overwrite ${sig_clip} --sigma ${sigma} --min_N ${min_N} --lst_start ${lst_start} --vis_units ${vis_units} --output_file_select ${output_file_select} ${data_files}
