69076756247ef287e26392e5b95f4f03ca9a0dbc
Paul La Plante <plaplant@sas.upenn.edu>
2018-06-20 09:15:47 -0400

    Add option for specifying number of batch jobs to submit simultaneously

--- scripts/makeflow_nrao.sh
+++ scripts/makeflow_nrao.sh
@@ -5,6 +5,10 @@
 # This command is makeflow_nrao.sh
 # Use this to run makeflow on the NRAO cluster environment
 # Several options are set for the execution on the cluster
+#
+# Args:
+#   1) name of makeflow file
+#   2) number of simultaneous jobs to submit (optional)
 
 # test that makeflow is on the PATH
 if ! [ -x "$(command -v makeflow)"  ]; then
@@ -18,7 +22,7 @@ if [ "$#" -lt 1 ]; then
     exit 2
 fi
 
-if ! [ "$(makeflow_analyze -k $1)" ]; then
+if ! [ "$(makeflow_analyze -k "${1}")" ]; then
     echo "Error: makeflow file is not parsable by makeflow; please contact hera_opm maintainer"
     exit 3
 fi
@@ -30,5 +34,9 @@ if [ "$(hostname)" != "herapost-master" ]; then
 fi
 
 # actually run makeflow
-# use cluster processing options
-makeflow -T torque $1
+# test to see if we have a second argument (number of jobs to submit simultaneously)
+if [ "$#" -gt 1 ]; then
+    makeflow -T torque -J "${2}" "${1}"
+else
+    makeflow -T torque "${1}"
+fi
