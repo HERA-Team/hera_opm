a3308067d4a0b3abe29010347c7db5cbbfa3263b
Josh Dillon <jsdillon@berkeley.edu>
2019-01-13 09:59:24 -0800

    add flag_nchan_low and flag_nchan_high to redcal options and DO script

--- pipelines/h1c/idr2/v2/task_scripts/do_REDCAL.sh
+++ pipelines/h1c/idr2/v2/task_scripts/do_REDCAL.sh
@@ -11,12 +11,16 @@ source ${src_dir}/_common.sh
 # 2 - directory of bad_ants files, with filename `{JD}.txt`
 # 3 - ant_z_thresh: Threshold of modified z-score for chi^2 per antenna above which antennas are thrown away and calibration is re-run iteratively.
 # 4 - solar_horizon: When the Sun is above this altitude in degrees, calibration is skipped and the integrations are flagged.
-# 5 - nInt_to_load: number of integrations to load and calibrate simultaneously. Lower numbers save memory, but incur a CPU overhead.
+# 5 - flag_nchan_low: integer number of channels at the low frequency end of the band to always flag (default 0)
+# 6 - flag_nchan_high: integer number of channels at the high frequency end of the band to always flag (default 0)
+# 7 - nInt_to_load: number of integrations to load and calibrate simultaneously. Lower numbers save memory, but incur a CPU overhead.
 fn="${1}"
 bad_ants_dir="${2}"
 ant_z_thresh="${3}"
 solar_horizon="${4}"
-nInt_to_load="${5}"
+flag_nchan_low="${5}"
+flag_nchan_high="${6}"
+nInt_to_load="${7}"
 
 # get metrics_json filename, removing extension and appending .ant_metrics.json
 nopol_base=$(remove_pol ${fn})
@@ -33,6 +37,6 @@ jd_int=`echo $jd | awk '{$1=int($1)}1'`
 bad_ants_fn=`echo "${bad_ants_dir}/${jd_int}.txt"`
 exants=$(prep_exants ${bad_ants_fn})
 
-echo redcal_run.py ${fn} --ex_ants ${exants} --ant_metrics_file ${metrics_f}  --ant_z_thresh ${ant_z_thresh} --solar_horizon ${solar_horizon} --nInt_to_load ${nInt_to_load} --clobber --verbose
-redcal_run.py ${fn} --ex_ants ${exants} --ant_metrics_file ${metrics_f}  --ant_z_thresh ${ant_z_thresh} --solar_horizon ${solar_horizon} --nInt_to_load ${nInt_to_load} --clobber --verbose
+echo redcal_run.py ${fn} --ex_ants ${exants} --ant_metrics_file ${metrics_f}  --ant_z_thresh ${ant_z_thresh} --solar_horizon ${solar_horizon} --flag_nchan_low ${flag_nchan_low} --flag_nchan_high ${flag_nchan_high} --nInt_to_load ${nInt_to_load} --clobber --verbose
+redcal_run.py ${fn} --ex_ants ${exants} --ant_metrics_file ${metrics_f}  --ant_z_thresh ${ant_z_thresh} --solar_horizon ${solar_horizon} --flag_nchan_low ${flag_nchan_low} --flag_nchan_high ${flag_nchan_high} --nInt_to_load ${nInt_to_load} --clobber --verbose
 
