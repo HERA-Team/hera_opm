8a8094b26f20c37b565fff6addb127a51e6ddec2
Adam Beardsley <adam.p.beardsley@gmail.com>
2019-01-31 15:08:36 -0700

    fix typo in cal_xrfi

--- pipelines/h1c/idr2/v2/task_scripts/do_CAL_XRFI.sh
+++ pipelines/h1c/idr2/v2/task_scripts/do_CAL_XRFI.sh
@@ -27,5 +27,5 @@ omni_calfits_file=`echo ${fn%.*}.omni.calfits`
 abs_calfits_file=`echo ${fn%.*}.abs.calfits`
 model_file=`echo ${fn%.*}.omni_vis.uvh5`
 
-echo cal_xrfi_run.py --omni_calfits_file=${omni_calfits_file} --abs_calfits_file=${abs_calfits_file} --model_file=${model_file} --metrics_ext=${2} --flag_ext=${3} --kt_size=${4} --kf_size=${5} --sig_init=${6} --sig_adj=${7} --freq_threshold=${8} --time_threshold=${9}
-cal_xrfi_run.py --omni_calfits_file=${omni_calfits_file} --abs_calfits_file=${abs_calfits_file} --model_file=${model_file} --metrics_ext=${2} --flag_ext=${3} --kt_size=${4} --kf_size=${5} --sig_init=${6} --sig_adj=${7} --freq_threshold=${8} --time_threshold=${9}
+echo cal_xrfi_run.py --omni_calfits_file=${omni_calfits_file} --abs_calfits_file=${abs_calfits_file} --model_file=${model_file} --metrics_ext=${2} --flags_ext=${3} --kt_size=${4} --kf_size=${5} --sig_init=${6} --sig_adj=${7} --freq_threshold=${8} --time_threshold=${9}
+cal_xrfi_run.py --omni_calfits_file=${omni_calfits_file} --abs_calfits_file=${abs_calfits_file} --model_file=${model_file} --metrics_ext=${2} --flags_ext=${3} --kt_size=${4} --kf_size=${5} --sig_init=${6} --sig_adj=${7} --freq_threshold=${8} --time_threshold=${9}
