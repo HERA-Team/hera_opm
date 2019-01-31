99b373dd9343d6d75d26399f0618f59df2bccd3a
Adam Beardsley <adam.p.beardsley@gmail.com>
2019-01-31 15:07:31 -0700

    remove ex_ants in cal_xrfi and delay_xrfi

--- pipelines/h1c/idr2/v2/task_scripts/do_CAL_XRFI.sh
+++ pipelines/h1c/idr2/v2/task_scripts/do_CAL_XRFI.sh
@@ -22,17 +22,10 @@ bn=$(basename ${1})  # Basename
 # 7 - sig_adj
 # 8 - freq_threshold
 # 9 - time_treshold
-# 10 - ex_ants_path
-
-# Get list of bad ants
-jd=$(get_jd ${bn})
-jd_int=`echo $jd | awk '{$1=int($1)}1'`
-bad_ants_fn=`echo "${2}/${jd_int}.txt"`
-ex_ants=$(prep_exants ${bad_ants_fn})
 
 omni_calfits_file=`echo ${fn%.*}.omni.calfits`
 abs_calfits_file=`echo ${fn%.*}.abs.calfits`
 model_file=`echo ${fn%.*}.omni_vis.uvh5`
 
-echo cal_xrfi_run.py --omni_calfits_file=${omni_calfits_file} --abs_calfits_file=${abs_calfits_file} --model_file=${model_file} --metrics_ext=${2} --flag_ext=${3} --kt_size=${4} --kf_size=${5} --sig_init=${6} --sig_adj=${7} --freq_threshold=${8} --time_threshold=${9} --ex_ants=${ex_ants}
-cal_xrfi_run.py --omni_calfits_file=${omni_calfits_file} --abs_calfits_file=${abs_calfits_file} --model_file=${model_file} --metrics_ext=${2} --flag_ext=${3} --kt_size=${4} --kf_size=${5} --sig_init=${6} --sig_adj=${7} --freq_threshold=${8} --time_threshold=${9} --ex_ants=${ex_ants}
+echo cal_xrfi_run.py --omni_calfits_file=${omni_calfits_file} --abs_calfits_file=${abs_calfits_file} --model_file=${model_file} --metrics_ext=${2} --flag_ext=${3} --kt_size=${4} --kf_size=${5} --sig_init=${6} --sig_adj=${7} --freq_threshold=${8} --time_threshold=${9}
+cal_xrfi_run.py --omni_calfits_file=${omni_calfits_file} --abs_calfits_file=${abs_calfits_file} --model_file=${model_file} --metrics_ext=${2} --flag_ext=${3} --kt_size=${4} --kf_size=${5} --sig_init=${6} --sig_adj=${7} --freq_threshold=${8} --time_threshold=${9}
