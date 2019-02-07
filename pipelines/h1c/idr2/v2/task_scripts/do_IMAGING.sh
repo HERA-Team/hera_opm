40f15146c27a3b6bebd144d76c93166e9c2f465a
Nicholas Kern <nkern@berkeley.edu>
2019-02-08 01:18:27 +0800

    modified:   idr2_2.toml 	modified:   task_scripts/do_IMAGING.sh

--- pipelines/h1c/idr2/v2/task_scripts/do_IMAGING.sh
+++ pipelines/h1c/idr2/v2/task_scripts/do_IMAGING.sh
@@ -8,24 +8,38 @@ source ${src_dir}/_common.sh
 # Parameters are set in the configuration file, here we define their positions,
 # which must be consistent with the config.
 # 1 - data filename
-# 2 - casa imaging scripts dir
-# 3 - spw selection
-# 4 - pixel size in arcsec
-# 5 - image size in Npixels
+# 2 - path to casa executable
+# 3 - casa imaging scripts dir
+# 4 - spw selection
+# 5 - pixel size in arcsec
+# 6 - image size in Npixels
+# 7 - N clean iterations
+# 8 - CLEAN stopping threshold
 filename="${1}"
-casa_imaging_scripts="${2}"
-spw="${3}"
-pxsize="${4}"
-imsize="${5}"
+casa="${2}"
+casa_imaging_scripts="${3}"
+spw="${4}"
+pxsize="${5}"
+imsize="${6}"
+niter="${7}"
+threshold="${8}"
 
 # convert file to uvfits
 echo convert_to_uvfits.py ${filename} --overwrite
 convert_to_uvfits.py ${filename} --overwrite
 
+# get uvfits and ms filename
+uvfits_file="${filename%.uvh5}.uvfits"
+ms_file="${filename%.uvh5}.ms"
+
+# make an imaging dir for outputs
+image_outdir="${filename}_image"
+mkdir $image_outdir
+
 # call sky_image.py from CASA_IMAGING package
-echo /home/casa/packages/RHEL7/release/casa-release-5.1.0-68/bin/casa -c ${casa_imaging_scripts}/sky_image.py --msin ${filename} --image_mfs --pxsize ${pxsize} --imsize ${imsize} --spw ${spw} --weighting briggs --robust 0 --export_fits
-/home/casa/packages/RHEL7/release/casa-release-5.1.0-68/bin/casa -c ${casa_imaging_scripts}/sky_image.py --msin ${filename} --image_mfs --pxsize ${pxsize} --imsize ${imsize} --spw ${spw} --weighting briggs --robust 0 --export_fits
+echo ${casa} -c ${casa_imaging_scripts}/sky_image.py --msin ${uvfits_file} --image_mfs --pxsize ${pxsize} --imsize ${imsize} --spw ${spw} --weighting briggs --robust 0 --export_fits --niter ${niter} --threshold ${threshold} --out_dir ${image_outdir}
+${casa} -c ${casa_imaging_scripts}/sky_image.py --msin ${uvfits_file} --image_mfs --pxsize ${pxsize} --imsize ${imsize} --spw ${spw} --weighting briggs --robust 0 --export_fits --niter ${niter} --threshold ${threshold} --out_dir ${image_outdir}
 
 # erase uvfits and MS file
-
-
+rm ${uvfits_file}
+rm -r ${image_outdir}/${ms_file}
