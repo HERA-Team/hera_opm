f0be85aa403f2ccb2f45de4ba3a48646807f7831
Paul La Plante <plaplant@sas.upenn.edu>
2019-03-14 16:17:46 -0600

    Update imaging step

--- pipelines/h1c/idr2/v2/task_scripts/do_IMAGING.sh
+++ pipelines/h1c/idr2/v2/task_scripts/do_IMAGING.sh
@@ -8,29 +8,18 @@ source ${src_dir}/_common.sh
 # Parameters are set in the configuration file, here we define their positions,
 # which must be consistent with the config.
 # 1 - data filename
-# 2 - calfits suffix
-# 3 - path to casa executable
-# 4 - casa imaging scripts dir
-# 5 - spw selection
-# 6 - pixel size in arcsec
-# 7 - image size in Npixels
-# 8 - N clean iterations
-# 9 - CLEAN stopping threshold
-# 10 - polarizations to image
+# 2 - path to casa executable
+# 3 - casa imaging scripts dir
+# 4 - calfits suffix (optional)
 filename="${1}"
-calibration="${2}"
-casa="${3}"
-casa_imaging_scripts="${4}"
-spw="${5}"
-pxsize="${6}"
-imsize="${7}"
-niter="${8}"
-threshold="${9}"
-stokes="${10}"
+casa="${2}"
+casa_imaging_scripts="${3}"
+if [ "$#" -ge 4 ]; then
+    calibration="${4}"
+fi
 
 # if calibration suffix is not empty, parse it and apply it
-if [ ! -z "${calibration}" ]
-then
+if [ ! -z "${calibration}" ]; then
     # parse calibration suffix
     cal_file="${filename%.uvh5}.${calibration}"
     echo apply_cal.py ${filename} ${filename}.calibrated --new_cal ${cal_file} --filetype_in uvh5 --filetype_out uvh5
@@ -43,23 +32,30 @@ echo convert_to_uvfits.py ${filename} --overwrite
 convert_to_uvfits.py ${filename} --overwrite
 
 # get uvfits and ms filename
-uvfits_file="${filename%.uvh5}.uvfits"
-ms_file="${filename%.uvh5}.ms"
+uvfits_file="${filename}.uvfits"
+uvfits_file_out="${filename}.image"
+ms_file="${filename}.ms"
+
+# get current directory
+cwd=`pwd`
 
 # make an imaging dir for outputs
 image_outdir="${filename}_image"
-mkdir $image_outdir
+mkdir -p ${image_outdir}
+cd ${image_outdir}
 
 # call sky_image.py from CASA_IMAGING package
-echo ${casa} -c ${casa_imaging_scripts}/sky_image.py --msin ${uvfits_file} --image_mfs --pxsize ${pxsize} --imsize ${imsize} --spw ${spw} --weighting briggs --robust 0 --export_fits --niter ${niter} --threshold ${threshold} --out_dir ${image_outdir} --stokes ${stokes}
-${casa} -c ${casa_imaging_scripts}/sky_image.py --msin ${uvfits_file} --image_mfs --pxsize ${pxsize} --imsize ${imsize} --spw ${spw} --weighting briggs --robust 0 --export_fits --niter ${niter} --threshold ${threshold} --out_dir ${image_outdir} --stokes ${stokes}
+echo ${casa} -c ${casa_imaging_scripts}/rtp_imaging.py --uvfitsname ${cwd}/${uvfits_file} --image ${uvfits_file_out}
+${casa} -c ${casa_imaging_scripts}/rtp_imaging.py --uvfitsname ${cwd}/${uvfits_file} --image ${uvfits_file_out}
 
 # erase uvfits and MS file
+cd ${cwd}
+echo rm ${uvfits_file}
 rm ${uvfits_file}
-rm -r ${image_outdir}/${ms_file}
+echo rm ${ms_file}
+rm -r ${ms_file}
 
 # remove calibrated visibility
-if [ ! -z "${calibration}" ]
-then
+if [ ! -z "${calibration}" ]; then
     rm filename
 fi
