8e50ced9d01ef69f4a1697b52b094d52a3784378
Josh Dillon <jsdillon@berkeley.edu>
2019-05-12 22:36:29 -0700

    Update smooth_cal for hera_cal/475
    This should be pulled in when https://github.com/HERA-Team/hera_cal/pull/475 is done.

--- pipelines/h1c/idr2/v2/task_scripts/do_CAL_SMOOTH.sh
+++ pipelines/h1c/idr2/v2/task_scripts/do_CAL_SMOOTH.sh
@@ -16,7 +16,9 @@ source ${src_dir}/_common.sh
 # 6 - window
 # 7 - maxiter
 # 8 - alpha
-# 9 - antflag_thresh
+# 9 - freq_threshold
+# 10 - time_threshold
+# 11 - ant_threshold
 
 fn="${1}"
 freq_scale="${2}"
@@ -26,7 +28,9 @@ filter_mode="${5}"
 window="${6}"
 maxiter="${7}"
 alpha="${8}"
-antflag_thresh="${9}"
+freq_threshold="${9}"
+time_threshold="${10}"
+ant_threshold="${11}"
 
 # get list of all calfiles for a day
 jd=$(get_jd $fn)
@@ -36,9 +40,13 @@ calfiles=`echo zen.${int_jd}.*.flagged_abs.calfits`
 # make the name of this calfits file for --run_if_first option
 this_calfile=`echo ${fn%.*}.flagged_abs.calfits`
 
-echo smooth_cal_run.py ${calfiles} --infile_replace .flagged_abs. --outfile_replace .smooth_abs. --clobber --antflag_thresh ${antflag_thresh} \
-                  --pick_refant --run_if_first ${this_calfile} --time_scale ${time_scale} --freq_scale ${freq_scale} --tol ${tol} \
-                  --filter_mode ${filter_mode} --window ${window} --maxiter ${maxiter} --alpha ${alpha} --verbose
-smooth_cal_run.py ${calfiles} --infile_replace .flagged_abs. --outfile_replace .smooth_abs. --clobber --antflag_thresh ${antflag_thresh} \
-                  --pick_refant --run_if_first ${this_calfile} --time_scale ${time_scale} --freq_scale ${freq_scale} --tol ${tol} \
-                  --filter_mode ${filter_mode} --window ${window} --maxiter ${maxiter} --alpha ${alpha} --verbose
+echo smooth_cal_run.py ${calfiles} --infile_replace .flagged_abs. --outfile_replace .smooth_abs. --clobber \
+                  --pick_refant --run_if_first ${this_calfile} --freq_scale ${freq_scale} \
+                  --time_scale ${time_scale} --tol ${tol} --filter_mode ${filter_mode} --window ${window} \
+                  --maxiter ${maxiter} --alpha ${alpha} --freq_threshold ${freq_threshold} \
+                  --time_threshold ${time_threshold} --ant_threshold ${ant_threshold} --verbose
+smooth_cal_run.py ${calfiles} --infile_replace .flagged_abs. --outfile_replace .smooth_abs. --clobber \
+                  --pick_refant --run_if_first ${this_calfile} --freq_scale ${freq_scale} \
+                  --time_scale ${time_scale} --tol ${tol} --filter_mode ${filter_mode} --window ${window} \
+                  --maxiter ${maxiter} --alpha ${alpha} --freq_threshold ${freq_threshold} \
+                  --time_threshold ${time_threshold} --ant_threshold ${ant_threshold} --verbose
