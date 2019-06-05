fcfc700a8f1b857736278cf47a0a3f8d662d297d
Nicholas Kern <nkern@berkeley.edu>
2019-06-05 15:56:38 -0600

    do_LSTBIN update with no cal fed

--- pipelines/h1c/idr2/v2/task_scripts/do_LSTBIN.sh
+++ pipelines/h1c/idr2/v2/task_scripts/do_LSTBIN.sh
@@ -42,7 +42,8 @@ data_files=($@)
 data_files=(${data_files[*]:14})
 
 # if calibration suffix is not empty, parse it and apply it
-if [ ! -z "${calibration}" ]; then
+if [ ! -z "${calibration}" ]
+then
     # parse calibration suffix for each nested list in data_files
     input_cals=()
     for df in "${data_files[@]}"; do
@@ -54,6 +55,9 @@ if [ ! -z "${calibration}" ]; then
         # add brackets
         input_cals+=("$ic")
     done
+    input_cals="--input_cals ${input_cals[@]}"
+else
+   input_cals="" 
 fi
 
 # set special kwargs
@@ -73,5 +77,5 @@ else
     fixed_lst_start=""
 fi
 
-echo lstbin_run.py --dlst ${dlst} --file_ext ${file_ext} --outdir ${outdir} --ntimes_per_file ${ntimes_per_file} ${rephase} ${sig_clip} --sigma ${sigma} --min_N ${min_N} --lst_start ${lst_start} ${fixed_lst_start} --vis_units ${vis_units} --output_file_select ${output_file_select} --Nbls_to_load ${Nbls_to_load} --input_cals ${input_cals[@]} --overwrite ${data_files[@]}
-lstbin_run.py --dlst ${dlst} --file_ext ${file_ext} --outdir ${outdir} --ntimes_per_file ${ntimes_per_file} ${rephase} ${sig_clip} --sigma ${sigma} --min_N ${min_N} --lst_start ${lst_start} ${fixed_lst_start} --vis_units ${vis_units} --output_file_select ${output_file_select} --Nbls_to_load ${Nbls_to_load} --input_cals ${input_cals[@]} --overwrite ${data_files[@]}
+echo lstbin_run.py --dlst ${dlst} --file_ext ${file_ext} --outdir ${outdir} --ntimes_per_file ${ntimes_per_file} ${rephase} ${sig_clip} --sigma ${sigma} --min_N ${min_N} --lst_start ${lst_start} ${fixed_lst_start} --vis_units ${vis_units} --output_file_select ${output_file_select} --Nbls_to_load ${Nbls_to_load} ${input_cals} --overwrite ${data_files[@]}
+lstbin_run.py --dlst ${dlst} --file_ext ${file_ext} --outdir ${outdir} --ntimes_per_file ${ntimes_per_file} ${rephase} ${sig_clip} --sigma ${sigma} --min_N ${min_N} --lst_start ${lst_start} ${fixed_lst_start} --vis_units ${vis_units} --output_file_select ${output_file_select} --Nbls_to_load ${Nbls_to_load} ${input_cals} --overwrite ${data_files[@]}
