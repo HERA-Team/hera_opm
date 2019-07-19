94f69b11177f8a83866eaa6ed60d23d783781102
Josh Dillon <jsdillon@berkeley.edu>
2019-07-19 16:13:26 -0700

    update for new xrfi_thresholding
    follows from https://github.com/HERA-Team/hera_qm/pull/296

--- pipelines/h1c/idr2/v2/task_scripts/do_XRFI_DAY_THRESHOLD.sh
+++ pipelines/h1c/idr2/v2/task_scripts/do_XRFI_DAY_THRESHOLD.sh
@@ -9,10 +9,10 @@ source ${src_dir}/_common.sh
 # which must be consistent with the config.
 # 1 - filename
 ### XRFI parameters - see hera_qm.utils for details
-# 2 - kt_size
-# 3 - kf_size
-# 4 - nsig_f
-# 5 - nsig_t
+# 2 - nsig_f
+# 3 - nsig_t
+# 4 - nsig_f_adj
+# 5 - nsig_t_adj
 
 fn="${1}"
 # get list of all data files for a day
@@ -20,5 +20,5 @@ jd=$(get_jd $fn)
 int_jd=${jd:0:7}
 data_files=`echo zen.${int_jd}.*.HH.uvh5`
 
-echo xrfi_day_threshold_run.py ${data_files} --kt_size=${2} --kf_size=${3} --nsig_f=${4} --nsig_t=${5} --clobber --run_if_first ${fn}
-xrfi_day_threshold_run.py ${data_files} --kt_size=${2} --kf_size=${3} --nsig_f=${4} --nsig_t=${5} --clobber --run_if_first ${fn}
+echo xrfi_day_threshold_run.py ${data_files} --nsig_f=${2} --nsig_t=${3} --nsig_f_adj=${4} --nsig_t_adj=${5} --clobber --run_if_first ${fn}
+xrfi_day_threshold_run.py ${data_files} --nsig_f=${2} --nsig_t=${3} --nsig_f_adj=${4} --nsig_t_adj=${5} --clobber --run_if_first ${fn}
