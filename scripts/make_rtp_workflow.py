535a66127a7b14a7f5e01ca021bba24840183028
Paul La Plante <plaplant@sas.upenn.edu>
2019-08-08 23:39:33 -0700

    Update RTP generation script

--- scripts/make_rtp_workflow.py
+++ scripts/make_rtp_workflow.py
@@ -5,8 +5,11 @@ from __future__ import print_function, division, absolute_import
 import os
 import time
 import datetime
+import subprocess
+
 from astropy.time import Time
 import redis
+
 from hera_opm import mf_tools as mt
 
 # hard-coded options
@@ -17,10 +20,11 @@ STORAGE_LOCATION = "/mnt/sn1"
 WORKFLOW_CONFIG = "/home/obs/src/hera_opm/pipelines/h3c/rtp/v1/rtp.toml"
 date = datetime.date.today().strftime("%y%m%d")
 MF_LOCATION = os.path.join("/home/obs/rtp_makeflow", date)
+CONDA_ENV = "RTP"
 
 # make a redis pool and initialize connection
 redis_pool = redis.ConnectionPool(REDIS_HOST)
-rsession = None
+rsession = redis.Redis(connection_pool=redis_pool)
 
 while True:
     time.sleep(MONITORING_INTERVAL)
@@ -45,8 +49,24 @@ while True:
         mf_name = "rtp_{}.mf".format(t0.fits)
         mf_path = os.path.join(MF_LOCATION, mf_name)
 
-        # make a workflow out of it
+        # make a workflow
         mt.build_makeflow_from_config(file_paths, WORKFLOW_CONFIG, mf_path, MF_LOCATION)
+
+        # launch workflow inside of screen
+        cmd = "conda activate {}; makeflow {}\n".format(CONDA_ENV, mf_path)
+        jd = jd0[:7]
+        screen_cmd1 = "screen -d -m -S {}".format(jd)
+        screen_cmd2 = "screen -S {} -p 0 -X stuff {}".format(jd, cmd)
+        try:
+            subprocess.check_call(screen_cmd1)
+            subprocess.check_call(screen_cmd2)
+        except subprocess.CalledProcessError as e:
+            raise ValueError(
+                "Error spawning screen session; command was {}; returncode was {:d}; "
+                "output was {}; stderr was {}".format(
+                    e.cmd, e.returncode, e.output, e.stderr
+                )
+            )
 
         # update redis
         rsession.hset("rtp:has_new_data", "state", "False")
