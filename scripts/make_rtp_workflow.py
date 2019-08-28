ba4f422a4f511c957cb54cfb74ea3fa5031116f0
Paul La Plante <plaplant@sas.upenn.edu>
2019-08-28 11:31:21 -0700

    Update rtp workflow script

--- scripts/make_rtp_workflow.py
+++ scripts/make_rtp_workflow.py
@@ -18,6 +18,7 @@ from hera_opm import mf_tools as mt
 # we'll do better some day...
 MONITORING_INTERVAL = 60
 REDIS_HOST = "redishost"
+REDIS_PORT = 6379
 STORAGE_LOCATION = "/mnt/sn1"
 WORKFLOW_CONFIG = "/home/obs/src/hera_opm/pipelines/h3c/rtp/v1/rtp.toml"
 date = datetime.date.today().strftime("%y%m%d")
@@ -25,7 +26,7 @@ MF_LOCATION = os.path.join("/home/obs/rtp_makeflow", date)
 CONDA_ENV = "RTP"
 
 # make a redis pool and initialize connection
-redis_pool = redis.ConnectionPool(REDIS_HOST)
+redis_pool = redis.ConnectionPool(host=REDIS_HOST, port=REDIS_PORT, db=0)
 rsession = redis.Redis(connection_pool=redis_pool)
 
 while True:
@@ -48,17 +49,17 @@ while True:
         # make a date stamp from the first file in the makeflow filename
         jd0 = float(new_files[0][4:17])
         t0 = Time(jd0, format="jd", out_subfmt="date_hms")
-        mf_name = "rtp_{}.mf".format(t0.fits)
+        mf_name = "rtp_{}.mf".format(t0.isot)
         mf_path = os.path.join(MF_LOCATION, mf_name)
 
         # make a workflow
         mt.build_makeflow_from_config(file_paths, WORKFLOW_CONFIG, mf_path, MF_LOCATION)
 
         # launch workflow inside of screen
-        cmd = "conda activate {}; makeflow {}\n".format(CONDA_ENV, mf_path)
-        jd = jd0[:7]
-        screen_cmd1 = "screen -d -m -S {}".format(jd)
-        screen_cmd2 = "screen -S {} -p 0 -X stuff {}".format(jd, cmd)
+        cmd = "conda activate {}; makeflow -T slurm {}\n".format(CONDA_ENV, mf_path)
+        screen_name = "rtp_{}".format(str(int(jd0)))
+        screen_cmd1 = ["screen", "-d", "-m", "-S", screen_name]
+        screen_cmd2 = ["screen", "-S", screen_name, "-p", "0", "-X", "stuff", cmd]
         try:
             subprocess.check_call(screen_cmd1)
             subprocess.check_call(screen_cmd2)
