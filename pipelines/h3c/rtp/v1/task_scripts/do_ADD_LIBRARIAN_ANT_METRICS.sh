24b7682cc01ee0ae3a14b1c083a9fb3c829d7677
Adam Beardsley <adam.p.beardsley@gmail.com>
2019-10-30 11:45:48 -0700

    Thread arguments for ant_metrics to toml

--- pipelines/h3c/rtp/v1/task_scripts/do_ADD_LIBRARIAN_ANT_METRICS.sh
+++ pipelines/h3c/rtp/v1/task_scripts/do_ADD_LIBRARIAN_ANT_METRICS.sh
@@ -8,13 +8,15 @@ source ${src_dir}/_common.sh
 # Parameters are set in the configuration file. Here we define their positions,
 # which must be consisten wtih the config.
 # 1 - (raw) filename
+# 2 - extension: Extension to be appended to the file name.
 fn=$(inject_hh ${1})
+extension=${2}
 
 # get the integer portion of the JD
 jd=$(get_int_jd ${1})
 
 # get ant_metrics filename
-metrics_f="${fn%.*}".ant_metrics.uvh5
+metrics_f="${fn%.*}"${extension}
 
 echo upload_to_librarian.py local-rtp ${metrics_f} ${jd}/${metrics_f}
 librarian upload local-rtp ${metrics_f} ${jd}/${metrics_f}
--- pipelines/h3c/rtp/v1/task_scripts/do_ANT_METRICS.sh
+++ pipelines/h3c/rtp/v1/task_scripts/do_ANT_METRICS.sh
@@ -8,7 +8,15 @@ source ${src_dir}/_common.sh
 # Parameters are set in the configuration file. Here we define their positions,
 # which must be consisten wtih the config.
 # 1 - (raw) filename
+# 2 - crossCut: Modified z-score cut for most cross-polarized antenna.
+# 3 - deadCut: Modified z-score cut for most likely dead antenna.
+# 4 - extension: Extension to be appended to the file name.
+# 5 - vis_format: File format for visibility files.
 fn=$(inject_hh ${1})
+crossCut=${2}
+deadCut=${3}
+extension=${4}
+vis_format=${5}
 
 # define polarizations
 pol1="xx"
@@ -19,5 +27,5 @@ pol4="yx"
 # make comma-separated list of polarizations
 pols=$(join_by , $pol1 $pol2 $pol3 $pol4)
 
-echo ant_metrics_run.py -p $pols --crossCut=5 --deadCut=5 --extension=.ant_metrics.hdf5 --vis_format=uvh5 $fn
-ant_metrics_run.py -p $pols --crossCut=5 --deadCut=5 --extension=.ant_metrics.hdf5 --vis_format=uvh5 $fn
+echo ant_metrics_run.py -p $pols --crossCut=${crossCut} --deadCut=${deadCut} --extension=${extension} --vis_format=${vis_format} $fn
+ant_metrics_run.py -p $pols --crossCut=${crossCut} --deadCut=${deadCut} --extension=${extension} --vis_format=${vis_format} $fn
