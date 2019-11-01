c1803512f3c051b74bcb873a6ef98757fb237f8a
HERA Observer <obs@qmaster>
2019-11-01 16:15:55 +0000

    rename action to pipeline_list since this is expected argument

--- pipelines/h3c/rtp/v1/task_scripts/do_ADD_RTP_PROCESS_RECORD.sh
+++ pipelines/h3c/rtp/v1/task_scripts/do_ADD_RTP_PROCESS_RECORD.sh
@@ -11,13 +11,13 @@ source ${src_dir}/_common.sh
 # 2 - List of actions from config file
 fn="${1}"
 # This script assumes there is only one non-file argument and assigns the rest
-# to actions
+# to pipeline_list
 shift
-actions="$@"
+pipeline_list="$@"
 # string-ify the arguments joining with ''
 # then remove "[", "]", "SETUP,", ",TEARDOWN", and ",ADD_RTP_PROCESS_RECORD"
-action_string=$(join_by ''  ${actions[@]} | sed -E "s/\[|\]|(SETUP,)|(,TEARDOWN)|(,ADD_RTP_PROCESS_RECORD)//g")
+action_string=$(join_by ''  ${pipeline_list[@]} | sed -E "s/\[|\]|(SETUP,)|(,TEARDOWN)|(,ADD_RTP_PROCESS_RECORD)//g")
 
 
-echo add_rtp_process_record.py ${fn} --actions ${action_string}
-add_rtp_process_record.py ${fn} --actions ${action_string}
+echo add_rtp_process_record.py ${fn} --pipeline_list ${action_string}
+add_rtp_process_record.py ${fn} --pipeline_list ${action_string}
