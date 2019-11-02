2fac2fae1c768d7a3e4254422b4a998494ee6f92
Josh Dillon <jsdillon@berkeley.edu>
2019-11-02 11:01:34 -0700

    update clean files script to target specific firstcal files and also remove ant_metrics

--- pipelines/h1c/idr3/v1/task_scripts/do_CLEAN_FILES.sh
+++ pipelines/h1c/idr3/v1/task_scripts/do_CLEAN_FILES.sh
@@ -8,6 +8,12 @@ source ${src_dir}/_common.sh
 # define input arguments
 fn="${1}"
 
+# remove antmetrics
+am_fn=$(remove_pol ${fn})
+am_fn=${am_fn}.ant_metrics.json
+echo rm -rfv ${am_fn}
+rm -rfv ${am_fn}
+
 # remove uvh5 files
 uvh5_fn=$(remove_pol ${fn})
 uvh5_fn=${uvh5_fn%.uv}.uvh5
@@ -15,5 +21,7 @@ echo rm -rfv ${uvh5_fn}
 rm -rfv ${uvh5_fn}
 
 # remove firstcal files
-echo rm -rfv *.first.calfits
-rm -rfv *.first.calfits
+firstcal_fn=${uvh5_fn%.uvh5}.first.calfits
+echo rm -rfv ${firstcal_fn}
+rm -rfv ${firstcal_fn}
+
