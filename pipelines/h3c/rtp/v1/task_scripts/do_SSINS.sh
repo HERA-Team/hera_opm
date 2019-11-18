ef9ea2df9103cfa3add34392bad42aedc5190b97
Mike Wilensky <mjw768@uw.edu>
2019-11-17 16:28:31 -0800

    Fix lonely do and typos

--- pipelines/h3c/rtp/v1/task_scripts/do_SSINS.sh
+++ pipelines/h3c/rtp/v1/task_scripts/do_SSINS.sh
@@ -6,7 +6,7 @@ src_dir="$(dirname "$0")"
 source ${src_dir}/_common.sh
 
 # Parameters are set in the configuration file. Here we define their positions,
-# which must be consisten wtih the config.
+# which must be consistent with the config.
 # 1 - raw filename
 # 2 - the significance threshold for streak shapes
 # 3 - the significance threshold to use for the other shapes
