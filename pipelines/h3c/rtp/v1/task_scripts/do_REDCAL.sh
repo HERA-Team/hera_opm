059498657d2ee64a6c2c4c4355de530cd9b6f933
Josh Dillon <jsdillon@berkeley.edu>
2020-02-12 15:36:58 -0800

    update documentation

--- pipelines/h3c/rtp/v1/task_scripts/do_REDCAL.sh
+++ pipelines/h3c/rtp/v1/task_scripts/do_REDCAL.sh
@@ -8,15 +8,18 @@ source ${src_dir}/_common.sh
 # Parameters are set in the configuration file, here we define their positions,
 # which must be consistent with the config.
 # 1 - filename
-# 2 - ant_z_thresh: Threshold of modified z-score for chi^2 per antenna above which antennas are thrown away and calibration is re-run iteratively.
-# 3 - solar_horizon: When the Sun is above this altitude in degrees, calibration is skipped and the integrations are flagged.
-# 4 - flag_nchan_low: integer number of channels at the low frequency end of the band to always flag (default 0)
-# 5 - flag_nchan_high: integer number of channels at the high frequency end of the band to always flag (default 0)
-# 6 - nInt_to_load: number of integrations to load and calibrate simultaneously. Lower numbers save memory, but incur a CPU overhead.
-# 7 - min_bl_cut: cut redundant groups with average baseline lengths shorter than this length in meters
-# 8 - max_bl_cut: cut redundant groups with average baseline lengths longer than this length in meters
-# 9 - iter0_prefix: if not "", save omnical results after the 0th interation with this prefix iff there will be a rerun
-# 10 - ant_metrics_extension: file extension to replace .uvh5 with to get ant_metrics files
+# 2 - directory of bad_ants files, with filename `{JD}.txt`
+# 3 - ant_z_thresh: Threshold of modified z-score for chi^2 per antenna above which antennas are thrown away and calibration is re-run iteratively.
+# 4 - solar_horizon: When the Sun is above this altitude in degrees, calibration is skipped and the integrations are flagged.
+# 5 - flag_nchan_low: integer number of channels at the low frequency end of the band to always flag (default 0)
+# 6 - flag_nchan_high: integer number of channels at the high frequency end of the band to always flag (default 0)
+# 7 - nInt_to_load: number of integrations to load and calibrate simultaneously. Lower numbers save memory, but incur a CPU overhead.
+# 8 - min_bl_cut: cut redundant groups with average baseline lengths shorter than this length in meters
+# 9 - max_bl_cut: cut redundant groups with average baseline lengths longer than this length in meters
+# 10 - iter0_prefix: if not "", save omnical results after the 0th interation with this prefix iff there will be a rerun
+# 11 - ant_metrics_extension: file extension to replace .uvh5 with to get ant_metrics files
+# 12 - good_statuses: string list of comma-separated (no spaces) antenna statuses considered good enough to try calibrating
+
 fn="${1}"
 bad_ants_dir="${2}"
 ant_z_thresh="${3}"
