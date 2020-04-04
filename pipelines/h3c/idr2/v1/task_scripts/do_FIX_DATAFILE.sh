9e3f8db099ddfc70b0eb70d9c4dfd1e4b3b7896c
Josh Dillon <jsdillon@berkeley.edu>
2020-04-04 11:22:36 -0700

    add fix_data_file for H3C IDR2 flag and nsamples problems

---
+++ pipelines/h3c/idr2/v1/task_scripts/do_FIX_DATAFILE.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+set -e
+
+# This function sets all the flags to False and all the nsamples to 1 and overwrites the file
+
+# import common functions
+src_dir="$(dirname "$0")"
+source ${src_dir}/_common.sh
+
+# Parameters are set in the configuration file, here we define their positions,
+# which must be consistent with the config.
+# 1 - filename
+
+fn="${1}"
+
+echo python -c "import numpy as np; \
+                from pyuvdata import UVData; \
+                uv = UVData(); \
+                uv.read("${fn}"); \
+                uv.flag_array = np.zeros_like(uv.flag_array); \
+                uv.nsample_array = np.ones_like(uv.nsample_array); \
+                uv.write_uvh5("${fn}", clobber=True)"
+python -c "import numpy as np; \
+           from pyuvdata import UVData; \
+           uv = UVData(); \
+           uv.read("${fn}"); \
+           uv.flag_array = np.zeros_like(uv.flag_array); \
+           uv.nsample_array = np.ones_like(uv.nsample_array); \
+           uv.write_uvh5("${fn}", clobber=True)"
