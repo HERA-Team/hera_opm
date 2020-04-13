ec2e15c490f35d5059e72cf43168dc9b60f4dd9b
Adam Beardsley <adam.p.beardsley@gmail.com>
2020-04-13 11:03:11 -0700

    add test to raise time_prereq error

--- hera_opm/tests/test_mf_tools.py
+++ hera_opm/tests/test_mf_tools.py
@@ -67,6 +67,9 @@ def config_options():
     config_dict["bad_missing_prereq_file"] = os.path.join(
         BAD_CONFIG_PATH, "bad_missing_prereq.toml"
     )
+    config_dict["bad_missing_time_prereq_file"] = os.path.join(
+        BAD_CONFIG_PATH, "bad_missing_time_prereq.toml"
+    )
     config_dict["obsids_pol"] = (
         "zen.2457698.40355.xx.HH.uvcA",
         "zen.2457698.40355.xy.HH.uvcA",
@@ -623,6 +626,26 @@ def test_build_analysis_makeflow_from_config_missing_prereq(config_options):
     if os.path.exists(outfile):
         os.remove(outfile)
     with pytest.raises(ValueError, match='Prereq FIRSTCAL_METRICS for action'):
+        mt.build_analysis_makeflow_from_config(obsids, config_file, work_dir=work_dir)
+
+    # clean up after ourselves
+    os.remove(outfile)
+    mt.clean_wrapper_scripts(work_dir)
+
+    return
+
+
+def test_build_analysis_makeflow_from_config_missing_time_prereq(config_options):
+    # define args
+    obsids = config_options["obsids_pol"][:1]
+    config_file = config_options["bad_missing_time_prereq_file"]
+    work_dir = os.path.join(DATA_PATH, "test_output")
+
+    mf_output = os.path.splitext(os.path.basename(config_file))[0] + ".mf"
+    outfile = os.path.join(work_dir, mf_output)
+    if os.path.exists(outfile):
+        os.remove(outfile)
+    with pytest.raises(ValueError, match='Time prereq GIBBLY_GOOP for action'):
         mt.build_analysis_makeflow_from_config(obsids, config_file, work_dir=work_dir)
 
     # clean up after ourselves
