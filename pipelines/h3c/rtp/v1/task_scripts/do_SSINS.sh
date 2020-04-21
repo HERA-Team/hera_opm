1e11633d93eedf2a7ffc93d69612bd01aa71b782
Mike Wilensky <mjw768@uw.edu>
2020-04-21 15:31:31 -0700

    Adjust argument ordering

--- pipelines/h3c/rtp/v1/task_scripts/do_SSINS.sh
+++ pipelines/h3c/rtp/v1/task_scripts/do_SSINS.sh
@@ -7,13 +7,16 @@ source ${src_dir}/_common.sh
 
 # Parameters are set in the configuration file. Here we define their positions,
 # which must be consistent with the config.
-# 1 - raw filename
-# 2 - the significance threshold for streak shapes
-# 3 - the significance threshold to use for the other shapes
-fn=${1}
-streak_sig=${2}
-other_sig=${3}
-N_samp_thresh=${4}
+
+# 1 - the significance threshold for streak shapes
+# 2 - the significance threshold to use for the other shapes
+# 3 - The threshold for flagging a highly contaminated frequency channel
+# 4 - The filename(s) to read in
+
+streak_sig=${1}
+other_sig=${2}
+N_samp_thresh=${3}
+fn=${4}
 
 # Get the right raw file
 prefix="${fn%.*}"
