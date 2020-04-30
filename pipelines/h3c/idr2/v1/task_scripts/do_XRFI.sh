8afc9ece892c3805494f11c36bcf7b737e3f93e2
Adam Beardsley <adam.p.beardsley@gmail.com>
2020-04-30 16:35:45 -0700

    xrfi scripts for h3c idr2.1

--- pipelines/h3c/idr2/v1/task_scripts/do_XRFI.sh
+++ pipelines/h3c/idr2/v1/task_scripts/do_XRFI.sh
@@ -4,24 +4,29 @@ set -e
 # import common functions
 src_dir="$(dirname "$0")"
 source ${src_dir}/_common.sh
-fn="${1}"  # Filename
-bn=$(basename ${1})  # Basename
+data_files="${@:5}"
 
 # We need to run xrfi on calibration outputs as preliminary flags before we
 # delay filter and run xrfi on visibilities.
 
 # Parameters are set in the configuration file, here we define their positions,
 # which must be consistent with the config.
-# 1 - filename
 ### XRFI parameters - see hera_qm.utils for details
-# 2 - kt_size
-# 3 - kf_size
-# 4 - sig_init
-# 5 - sig_adj
+# 1 - kt_size
+# 2 - kf_size
+# 3 - sig_init
+# 4 - sig_adj
+# 5+ - filenames
 
-ocalfits_file=`echo ${fn%.*}.omni.calfits`
-acalfits_file=`echo ${fn%.*}.abs.calfits`
-model_file=`echo ${fn%.*}.omni_vis.uvh5`
+ocalfits_files=''
+acalfits_files=''
+model_files=''
+for data_file in ${data_files[@]}; do
+  # Append list of cal files
+  ocalfits_files=${ocalfits_files}${data_file%.*}.omni.calfits' '
+  acalfits_files=${acalfits_files}${data_file%.*}.abs.calfits' '
+  model_files=${model_files}${data_file%.*}.omni_vis.uvh5' '
+done
 
-echo xrfi_run.py --ocalfits_file=${ocalfits_file} --acalfits_file=${acalfits_file} --model_file=${model_file} --data_file=${fn} --kt_size=${2} --kf_size=${3} --sig_init=${4} --sig_adj=${5} --clobber
-xrfi_run.py --ocalfits_file=${ocalfits_file} --acalfits_file=${acalfits_file} --model_file=${model_file} --data_file=${fn} --kt_size=${2} --kf_size=${3} --sig_init=${4} --sig_adj=${5} --clobber
+echo xrfi_h3c_idr2_1_run.py --ocalfits_files=${ocalfits_files} --acalfits_files=${acalfits_files} --model_files=${model_files} --data_files=${data_files} --kt_size=${1} --kf_size=${2} --sig_init=${3} --sig_adj=${4} --clobber
+xrfi_run.py --ocalfits_files=${ocalfits_files} --acalfits_files=${acalfits_files} --model_files=${model_files} --data_files=${data_files} --kt_size=${1} --kf_size=${2} --sig_init=${3} --sig_adj=${4} --clobber
