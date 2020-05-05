e2bf9ad8050619066507616bd56bca6931e8bf9a
Adam Beardsley <adam.p.beardsley@gmail.com>
2020-05-05 10:07:44 -0700

    add python script for fixing h3c data files

---
+++ scripts/fix_h3c_data_file.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- mode: python; coding: utf-8 -*-
+# Copyright (c) 2020 The HERA Collaboration
+# Licensed under the 2-clause BSD License
+"""Script for fixing some issues with H3C data files and possibly renaming."""
+
+import numpy as np
+from pyuvdata import UVData
+import argparse
+import os
+
+ap = argparse.ArgumentParser()
+ap.prog = 'fix_h3c_data_file.py'
+ap.add_argument('filenames', metavar='filenames', nargs='*', type=str, default=[],
+                help='File to correct flag_array and nsample_array.')
+args = ap.parse_args()
+
+uv = UVData()
+
+for filename in args.filenames:
+    uv.read(filename)
+    uv.flag_array = np.zeros_like(uv.flag_array)
+    uv.nsample_array = np.ones_like(uv.nsample_array)
+    uv.history += 'Corrected flag_array and nsmaple_array with hera_opm/fix_h3c_data_file.py'
+    if not 'sum' in filename:
+        # Rename file to convention zen.JD.JD.sum.uvh5)
+        dirname = os.path.dirname(os.path.abspath(filename))
+        new_filename = '.'.join(os.path.basename(filename).split('.')[0:3] + ['sum', 'uvh5']
+        print(f'changing file {filename} to {new_filename}')
+        uv.write_uvh5(os.path.join(dirname, new_filename), clobber=True)
+        os.remove(filename)
+    else:
+        uv.write_uvh5(filename, clobber=True)
