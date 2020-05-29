84ff7b2c8131d3d89156b40fc7b59b7f6082cc1a
Josh Dillon <jsdillon@berkeley.edu>
2020-05-28 18:07:17 -0700

    formatting fixes

--- pipelines/h1c/idr2/v2/task_scripts/do_LSTBIN.sh
+++ pipelines/h1c/idr2/v2/task_scripts/do_LSTBIN.sh
@@ -56,7 +56,7 @@ then
     done
     input_cals="--input_cals ${input_cals[@]}"
     # if there's a calibration string, then the data files start at the 15th position
-    data_files=(${data_files[*]:14}) 
+    data_files=(${data_files[*]:14})
 else
    input_cals=""
    # if there's no calibration string, then they start at the 14th position
