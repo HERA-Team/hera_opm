4b8d493ae434126ff88c1248971600ab98a0ff13
Aaron Ewall-Wice <aaronew@berkeley.edu>
2020-06-28 12:59:29 -0700

    Get rid of partial-io for delay filtering (for now).

--- pipelines/h3c/idr2/v1/task_scripts/do_DELAY.sh
+++ pipelines/h3c/idr2/v1/task_scripts/do_DELAY.sh
@@ -9,23 +9,20 @@ source ${src_dir}/_common.sh
 # which must be consistent with the config.
 # 1 - filename
 # 2 - calibration file
-# 3 - number of baselines to partially load and filter
-#     at a time
-# 4 - extra label for the output file.
-# 5 - spw0 lower channel to process.
-# 6 - spw1 upper channel to process.
-# 7 - tol level to subtract foregrounds too
-# 8 - standoff delay standoff in ns for filtering window.
-# 9 - cache_dir, directory to store cache files in.
+# 3 - extra label for the output file.
+# 4 - spw0 lower channel to process.
+# 5 - spw1 upper channel to process.
+# 6 - tol level to subtract foregrounds too
+# 7 - standoff delay standoff in ns for filtering window.
+# 8 - cache_dir, directory to store cache files in.
 fn="${1}"
 calibration="${2}"
-nbls_partial="${3}"
-label="${4}"
-spw0="${5}"
-spw1="${6}"
-tol="${7}"
-standoff="${8}"
-cache_dir="${9}"
+label="${3}"
+spw0="${4}"
+spw1="${5}"
+tol="${6}"
+standoff="${7}"
+cache_dir="${8}"
 # get julian day from file name
 jd=$(get_jd $fn)
 # generate output file name
@@ -36,10 +33,10 @@ if [ ! -d "${cache_dir}" ]; then
 fi
 calfile=${fn%.uvh5}.${calibration}
 
-echo dayenu_delay_filter_run.py ${fn} --calfile ${calfile} --partial_load_Nbls ${nbls_partial} \
+echo dayenu_delay_filter_run.py ${fn} --calfile ${calfile} \
   --res_outfilename ${fn_out} --clobber --spw_range ${spw0} ${spw1} \
   --tol ${tol} --cache_dir ${cache_dir} --standoff ${standoff} --write_cache --read_cache
 
-dayenu_delay_filter_run.py ${fn} --calfile ${calfile} --partial_load_Nbls ${nbls_partial} \
+dayenu_delay_filter_run.py ${fn} --calfile ${calfile} \
     --res_outfilename ${fn_out} --clobber --spw_range ${spw0} ${spw1} \
     --tol ${tol} --cache_dir ${cache_dir} --standoff ${standoff} --write_cache --read_cache
