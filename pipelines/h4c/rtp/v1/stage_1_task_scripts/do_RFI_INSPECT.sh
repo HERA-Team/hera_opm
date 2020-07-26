0f7c1cb2116e1de60ce68012076157250a7975da
Josh Dillon <jsdillon@berkeley.edu>
2020-07-25 19:14:06 -0700

    add placeholder for rfi_inspect notebook

---
+++ pipelines/h4c/rtp/v1/stage_1_task_scripts/do_RFI_INSPECT.sh
@@ -0,0 +1,46 @@
+set -e
+
+# This script generates a notebook for inspecting the results of data-only RFI identification
+
+# TODO: THIS IS CURRENTLY A PLACEHOLDER
+echo "THIS IS A PLACEHOLDER FOR A SCRIPT THAT RUNS THE rfi_inspect NOTEBOOK. NOT CURRENTLY IMPLEMENTED."
+# TODO: uncomment below when rfi_inspect.ipynb is ready
+
+# src_dir="$(dirname $0)"
+# source ${src_dir}/_common.sh
+
+# # Parameters are set in the configuration file. Here we define their positions,
+# # which must be consistent with the config.
+# # 1 - (raw) filename
+# # 2 - nb_template_dir: where to look for the notebook template
+# # 3 - nb_output_repo: repository for saving evaluated notebooks
+# # 4 - git_push: boolean whether to push the results created in the nb_output_repo
+# fn=${1}
+# nb_template_dir=${2}
+# nb_output_repo=${3}
+# git_push=${4}
+
+# # Get JD from filename
+# jd=$(get_int_jd ${fn})
+# nb_outfile=${nb_output_repo}/rfi_inspect/rfi_inspect_${jd}.ipynb
+
+# # Export variables used by the notebook
+# export DATA_PATH=`pwd`
+# export JULIANDATE=${jd}
+
+# # Execute jupyter notebook
+# jupyter nbconvert --output=${nb_outfile} \
+# --to notebook \
+# --ExecutePreprocessor.allow_errors=True \
+# --ExecutePreprocessor.timeout=-1 \
+# --execute ${nb_template_dir}/rfi_inspect.ipynb
+
+# # If desired, push results to github
+# if ${git_push}
+# then
+#     cd ${nb_output_repo}
+#     git pull origin master
+#     git add ${nb_outfile}
+#     git commit -m "RTP RFI inspection notebook commit for JD ${jd}"
+#     git push origin master
+# fi
