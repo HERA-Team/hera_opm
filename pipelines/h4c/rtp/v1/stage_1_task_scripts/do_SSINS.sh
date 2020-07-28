d981a3cf5672bf11866df2351f53fd6aff6cbdff
Mike Wilensky <mjw768@uw.edu>
2020-07-28 11:18:11 -0700

    delete junk toml and copy task script over to h4c

---
+++ pipelines/h4c/rtp/v1/stage_1_task_scripts/do_SSINS.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+# import common functions
+src_dir="$(dirname "$0")"
+source ${src_dir}/_common.sh
+
+# Parameters are set in the configuration file. Here we define their positions,
+# which must be consistent with the config.
+
+# 1 - the significance threshold for streak shapes
+# 2 - the significance threshold to use for the other shapes
+# 3 - The threshold for flagging a highly contaminated frequency channel
+# 4 - The filename(s) to read in
+
+all_args=("$@")
+
+streak_sig=${1}
+other_sig=${2}
+tb_aggro=${3}
+fns=("${all_args[@]:3}")
+
+
+echo filenames are "${fns[@]}"
+
+# Get the first filename in the list (should work if only one file)
+first_file="${fns%" "*}"
+# Set the prefix based on the first filename
+prefix="${first_file%.*}"
+
+echo Run_HERA_SSINS.py -f "${fns[@]}" -s $streak_sig -o $other_sig -p $prefix -t $tb_aggro -c
+Run_HERA_SSINS.py -f "${fns[@]}" -s $streak_sig -o $other_sig -p $prefix -t $tb_aggro -c
