94602f68e365f8474871c049a1998ef18df916e9
Josh Dillon <jsdillon@berkeley.edu>
2020-07-30 15:55:30 -0700

    add verbosity

--- pipelines/h4c/rtp/v1/stage_1_task_scripts/do_XRFI_DAY_THRESHOLD.sh
+++ pipelines/h4c/rtp/v1/stage_1_task_scripts/do_XRFI_DAY_THRESHOLD.sh
@@ -23,9 +23,11 @@ xrfi_day_threshold_run.py ${data_files} --skip_making_flagged_abs_calfits --nsig
 # Rename results so that there will be no conflict with stage 2
 for df in ${data_files}
 do
+    echo mv ${df%.sum.uvh5}.xrfi  ${df%.sum.uvh5}.stage_1_xrfi
     mv ${df%.sum.uvh5}.xrfi  ${df%.sum.uvh5}.stage_1_xrfi
 done
 for ff in *_threshold_flags.h5.h5
 do
+    echo mv ${ff} ${ff%_threshold_flags.h5}_stage_1_threshold_flag.h5
     mv ${ff} ${ff%_threshold_flags.h5}_stage_1_threshold_flag.h5
 done
