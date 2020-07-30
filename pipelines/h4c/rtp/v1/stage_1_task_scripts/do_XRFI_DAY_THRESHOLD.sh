892733b216a2907b295787fc998464bd4355aaf7
Josh Dillon <jsdillon@berkeley.edu>
2020-07-30 16:21:49 -0700

    remove renaming completely

--- pipelines/h4c/rtp/v1/stage_1_task_scripts/do_XRFI_DAY_THRESHOLD.sh
+++ pipelines/h4c/rtp/v1/stage_1_task_scripts/do_XRFI_DAY_THRESHOLD.sh
@@ -21,13 +21,13 @@ echo xrfi_day_threshold_run.py ${data_files} --skip_making_flagged_abs_calfits -
 xrfi_day_threshold_run.py ${data_files} --skip_making_flagged_abs_calfits --nsig_f=${1} --nsig_t=${2} --nsig_f_adj=${3} --nsig_t_adj=${4} --clobber
 
 # Rename results so that there will be no conflict with stage 2
-for df in ${data_files}
-do
-    echo mv ${df%.sum.uvh5}.xrfi  ${df%.sum.uvh5}.stage_1_xrfi
-    mv ${df%.sum.uvh5}.xrfi ${df%.sum.uvh5}.stage_1_xrfi
-done
-for ff in *_threshold_flags.h5.h5
-do
-    echo mv ${ff} ${ff%_threshold_flags.h5}_stage_1_threshold_flag.h5
-    mv ${ff} ${ff%_threshold_flags.h5}_stage_1_threshold_flag.h5
-done
+# for df in ${data_files}
+# do
+#     echo mv ${df%.sum.uvh5}.xrfi  ${df%.sum.uvh5}.stage_1_xrfi
+#     mv ${df%.sum.uvh5}.xrfi ${df%.sum.uvh5}.stage_1_xrfi
+# done
+# for ff in *_threshold_flags.h5.h5
+# do
+#     echo mv ${ff} ${ff%_threshold_flags.h5}_stage_1_threshold_flag.h5
+#     mv ${ff} ${ff%_threshold_flags.h5}_stage_1_threshold_flag.h5
+# done
