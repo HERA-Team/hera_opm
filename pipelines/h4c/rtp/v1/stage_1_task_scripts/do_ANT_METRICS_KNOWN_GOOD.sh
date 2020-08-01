73e166f08e192a97489acfcea03b85380c0df0a0
Josh Dillon <jsdillon@berkeley.edu>
2020-08-01 14:16:32 -0700

    try chunking ant_metrics

--- pipelines/h4c/rtp/v1/stage_1_task_scripts/do_ANT_METRICS_KNOWN_GOOD.sh
+++ pipelines/h4c/rtp/v1/stage_1_task_scripts/do_ANT_METRICS_KNOWN_GOOD.sh
@@ -9,25 +9,26 @@ source ${src_dir}/_common.sh
 
 # Parameters are set in the configuration file. Here we define their positions,
 # which must be consistent with the config.
-# 1 - (raw) filename
-# 2 - crossCut: Modified z-score cut for most cross-polarized antenna.
-# 3 - deadCut: Modified z-score cut for most likely dead antenna.
-# 4 - Nbls_per_load: Number of baselines to load simultaneously.
-# 5 - extension: Extension to be appended to the file name.
-# 6 - known_good_statuses: string list of comma-separated (no spaces) antenna statuses that represent "good" antennas
-fn=${1}
-crossCut=${2}
-deadCut=${3}
-Nbls_per_load=${4}
-extension=${5}
-known_good_statuses=${6}
+# 1 - crossCut: Modified z-score cut for most cross-polarized antenna.
+# 2 - deadCut: Modified z-score cut for most likely dead antenna.
+# 3 - Nbls_per_load: Number of baselines to load simultaneously.
+# 4 - extension: Extension to be appended to the file name.
+# 5 - known_good_statuses: string list of comma-separated (no spaces) antenna statuses that represent "good" antennas
+# 6+ - filenames
+crossCut=${1}
+deadCut=${2}
+Nbls_per_load=${3}
+extension=${4}
+known_good_statuses=${5}
+fn1="${6}"
+data_files="${@:6}"
 
 # get exants from HERA CM database
-jd=$(get_jd ${fn})
+jd=$(get_jd ${fn1})
 apriori_xants=`query_ex_ants.py ${jd} ${known_good_statuses}`
 
 # We only want to run ant metrics on sum files
-echo ant_metrics_run.py $fn --crossCut ${crossCut} --deadCut ${deadCut} --extension ${extension} --Nbls_per_load ${Nbls_per_load} \
+echo ant_metrics_run.py ${data_files} --crossCut ${crossCut} --deadCut ${deadCut} --extension ${extension} --Nbls_per_load ${Nbls_per_load} \
     --clobber --apriori_xants ${apriori_xants}
-ant_metrics_run.py $fn --crossCut ${crossCut} --deadCut ${deadCut} --extension ${extension} --Nbls_per_load ${Nbls_per_load} \
+ant_metrics_run.py ${data_files} --crossCut ${crossCut} --deadCut ${deadCut} --extension ${extension} --Nbls_per_load ${Nbls_per_load} \
     --clobber --apriori_xants ${apriori_xants}
