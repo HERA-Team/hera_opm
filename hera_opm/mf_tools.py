cb258b7311a513aa97144382baa06c16500d280e
Paul La Plante <plaplant@sas.upenn.edu>
2021-04-18 11:32:05 -0700

    Add test for new functionality

--- hera_opm/mf_tools.py
+++ hera_opm/mf_tools.py
@@ -26,7 +26,7 @@ def get_jd(filename):
 
     Returns
     -------
-    str
+    str or None
         The integer JD (fractional part truncated). Returns None if
         filename does not match assumed format.
 
